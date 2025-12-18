19b03326cccba9a7eed9dce362f88fae646c8302
ABCG <abcg@192.168.1.15>
2025-12-18 19:23:39 +0500

    Fix Sanity Project ID fallback format

--- sanity/env.ts
+++ sanity/env.ts
@@ -2,5 +2,5 @@ export const apiVersion =
     process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-12-18'
 
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production';
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'dummy_project_id';
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'dummyprojectid';
 export const useCdn = false
